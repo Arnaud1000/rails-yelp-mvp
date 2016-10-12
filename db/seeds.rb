21fca3a0fc9b2196a074d9b5c048dd02960b1a4d
Arnaud COUESNON <arnaud.couesnon@gmail.com>
2016-10-12 16:31:01 +0200

    Reviews and Restaurants 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Restaurant.destroy_all
 5.times do
-restaurants = Restaurant.create(name: Faker::Company.name , address: Faker::Address.street_address , stars: [1,2,3,4,5].sample, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone:Faker::PhoneNumber.phone_number)
+restaurants = Restaurant.create(name: Faker::Pokemon.name , address: Faker::Pokemon.location , stars: [1,2,3,4,5].sample, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone:Faker::PhoneNumber.phone_number, description:Faker::ChuckNorris.fact)
 end
 p Restaurant.count
